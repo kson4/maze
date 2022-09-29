57c12d936bf90c4a68f5bd05a2076a97d82e18ca
david <davidsinternetmail@gmail.com>
2022-09-29 12:29:12 -0700

    settings menu, adjustable rows and cols

--- js/script.js
+++ js/script.js
@@ -8,3 +8,20 @@ document.querySelector("#settings-icon").addEventListener("click", () => {
   document.querySelector(".maze-container").classList.add("inactive")
   console.log("clicked")
 })
+
+// settings menu slider
+const sliders = document.querySelectorAll(".slider")
+const values = document.querySelectorAll(".value")
+const progress = document.querySelectorAll(".progres")
+for (let i = 0; i < sliders.length; i++) {
+  values[i].textContent = sliders[i].value
+}
+for (let i = 0; i < sliders.length; i++) {
+  sliders[i].oninput = (() => {
+    values[i].textContent = sliders[i].value
+    values[i].value = sliders[i].value
+  })
+  values[i].oninput = (() => {
+    sliders[i].value = values[i].value
+  })
+}
